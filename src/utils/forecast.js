e9c1f3af099dea5ee4d826147daeb8b4111edf88
Mitchell Van Asten <mitchellvanasten2@gmail.com>
2021-09-07 16:19:43 +1000

    added wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' Today. It is currently ' + body.current.temperature + ' Degrees out, however It feels like '+ body.current.feelslike +'. There is a '+ body.current.precip+' % chance to rain')
+            //console.log(body.current.data[0])
+            callback(undefined, body.current.weather_descriptions[0] + ' Today. It is currently ' + body.current.temperature + ' Degrees out, however It feels like '+ body.current.feelslike +'. There is a '+ body.current.precip+' % chance to rain. The windspeed is ' + body.current.wind_speed)
         } 
         
     })
